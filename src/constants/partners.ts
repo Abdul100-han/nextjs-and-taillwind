049b445fa2b96964b628317daf4603b6818c302e
Abdul100-han <abdulmuhd857@gmail.com>
2025-04-16 17:37:51 +0100

    defined partner constants

---
+++ src/constants/partners.ts
@@ -0,0 +1,11 @@
+import { Partner } from "@/types";
+
+export const partners: Partner[] = [
+  {
+    id: "1",
+    name: "TechCorp",
+    logo: "/logos/techcorp.png",
+    url: "https://techcorp.com",
+  },
+  // Add more partners...
+];
