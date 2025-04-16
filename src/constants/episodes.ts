cf6451e530ee4ab3d500ae3810ff6e1ab3c23193
Abdul100-han <abdulmuhd857@gmail.com>
2025-04-16 17:36:44 +0100

    defined episode constants

---
+++ src/constants/episodes.ts
@@ -0,0 +1,14 @@
+import { Episode } from "@/types";
+
+export const episodes: Episode[] = [
+  {
+    id: "1",
+    title: "The Future of Web Development",
+    description: "Exploring Next.js 14 and beyond",
+    image: "/images/episode1.jpg",
+    duration: "32:15",
+    audioUrl: "/audio/future-web.mp3",
+    publishDate: "2023-06-10",
+  },
+  // Add more episodes...
+];
