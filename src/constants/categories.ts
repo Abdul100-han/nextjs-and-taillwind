41a3feba67b61256d85ccaa1e4b03620f67eac66
Abdul100-han <abdulmuhd857@gmail.com>
2025-04-16 17:37:16 +0100

    defined categories constants

---
+++ src/constants/categories.ts
@@ -0,0 +1,11 @@
+import { Category } from "@/types";
+
+export const categories: Category[] = [
+  {
+    id: "1",
+    name: "News & Storytelling",
+    image: "/images/category1.jpg",
+    count: 24,
+  },
+  // Add more categories...
+];
